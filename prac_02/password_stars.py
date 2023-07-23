5f02720226e378ea1c3f290731c638f79b6eab8b
lipeipei <peipei0814i@gmail.com>
2023-07-23 19:02:42 +0800

    Completion of password_stars.py file reconstruction

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,13 +1,32 @@
+"""
+CP1404
+Password_stars.py
+Password Check with Functions
+"""
 MINIMUM_LENGTH = 5
 
-password = input("Enter your password: ")
 
-while len(password) < MINIMUM_LENGTH:
-
-    print("Password doesn't meet a minimum length")
-
+def get_password():
+    """Get the entered password and verify it"""
     password = input("Enter your password: ")
+    while len(password) < MINIMUM_LENGTH:
+        print("Password doesn't meet the minimum length")
+        password = input("Enter your password: ")
+    return password
 
-print(len(password) * "*")
+
+def print_asterisks(password):
+    """Print asterisks"""
+    print(len(password) * "*")
+
+
+def main():
+    """main function"""
+    password = get_password()
+    print_asterisks(password)
+
+
+main()
+
 
 
