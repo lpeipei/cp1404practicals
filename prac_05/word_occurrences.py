de22c4020054d77b2d2f423a5196ed182c394bd9
lipeipei <peipei0814i@gmail.com>
2023-08-13 18:04:39 +0800

    Complete prac 5 word occurrences exercise

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,21 @@
+def main():
+    text = input("Enter your text: ")
+    word_count = count_word_occurrences(text)
+    max_word_length = max(len(word) for word in word_count.keys())
+
+    for word, count in sorted(word_count.items()):
+        print(f"{word:{max_word_length}} : {count}")
+
+
+def count_word_occurrences(input_string):
+    words = input_string.lower().split()
+    word_count = {}
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    return word_count
+
+
+main()
